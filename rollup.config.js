09ee9a148ae6f6c258d46e72f4fb46a5fb694c0c
Braydon Kains <kainsbraydon@gmail.com>
2020-08-21 11:22:44 -0400

    Added single file and multi file bundle configurations (#1)
    * Added single file and multi file bundle configurations
    
    * Cleaned up the examples to not actually include anything, just some example comment lines
    
    * Fixed verbiage of build types to 'bundled' and 'unbundled', cleaned up some writing, removed package-lock
    
    * Semver 1

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import { createBasicConfig } from '@open-wc/building-rollup';
+import merge from 'deepmerge';
+import resolve from 'rollup-plugin-node-resolve';
+
+const componentFiles = [
+	// Example
+	// './node_modules/@brightspace-ui/core/components/button/button.js',
+	// './node_modules/@brightspace-ui/core/components/loading-spinner/loading-spinner.js',
+];
+
+const config = createBasicConfig({
+	developmentMode: false, /* forces tree-shaking, minify ON */
+	legacyBuild: true, /* required for IE11 and legacy-Edge support */
+	outputDir: 'build',
+});
+config.input = componentFiles;
+config.output[0].entryFileNames = '[name].js';
+config.output[0].chunkFileNames = '[name].js';
+config.output[1].entryFileNames = 'nomodule-[name].js';
+config.output[1].chunkFileNames = 'nomodule-[name].js';
+
+export default merge(config, {
+	plugins: [
+		resolve()
+	]
+});
